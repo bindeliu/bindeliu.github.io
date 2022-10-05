08f8061a80d22a7f842ec2837d1d8b5f2099a290
bindeliu <hysuperliu@foxmail.com>
2022-10-05 17:40:54 +0800

    fix: 优化体验

--- src/jocker/muyu.tsx
+++ src/jocker/muyu.tsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import muyuBgm from "../static/muyu.mp3";
 import { useAudio } from "../hooks/play";
 import "./muyu.less";
 
 const clickNumberFromCache = Number(localStorage.getItem("clickNumber")) || 0;
-
 const Muyu = () => {
+  
   const [clickState, setClickState] = useState(false);
   const [clickNumber, setClickNumber] = useState(clickNumberFromCache);
   const [audioState, audioToggle] = useAudio(muyuBgm, { loop: false });
+
+  let a:any = null
+  let [autoClickInterval,setAutoClickInterval] = useState(a)
+
+ 
 
   const clickMuyu = () => {
     console.log(clickState,audioState)
@@ -24,6 +29,10 @@ const Muyu = () => {
       setClickState(false);
     }, 200);
   };
+  const ref = useRef(clickMuyu)
+  ref.current = () => {
+    clickMuyu()
+  };
 
   const clearNumber = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -36,23 +45,40 @@ const Muyu = () => {
     display: clickState ? "block" : "none",
   };
 
+  const autoClick = ()=>{
+    console.warn(autoClickInterval)
+    if(autoClickInterval){
+      clearInterval(autoClickInterval);
+      setAutoClickInterval(null)
+    }else{
+      setAutoClickInterval(setInterval(()=>{
+        ref.current()
+      },500))
+    }
+     
+
+  }
+ 
+  const clickText = clickNumber ? <p>您已敲击 <span className="count"> { clickNumber } </span>  次</p>  :  "请诚心敲击木鱼"
   return (
     <div
       className={["muyu-container", clickState ? "clicked" : ""].join(" ")}
       onClick={clickMuyu}
     >
       <div className="muyu-count">
-        {clickNumber ? `您已敲击${clickNumber}次` : "请诚心敲击木鱼"}
-      </div>
+        {clickText}
+      </div> 
       <div className="muyu-text" style={textShow}>
         佛缘+1
       </div>
       <div className={["muyu", clickState ? "clicked" : ""].join(" ")}></div>
-      {/* <div className="btn" onClick={clearNumber}>
-        计数清0
-      </div> */}
+      <div className="auto" onClick={autoClick}>
+        挂机
+      </div>
     </div>
   );
 };
 
 export default Muyu;
+
+
